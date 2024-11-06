795aca1cdccf61a7e915e9e59bd4543221b761ec
Zalanskiy Nikita <1985gramaboy@gmail.com>
2024-11-06 14:37:13 +0200

    Design v1

---
+++ Visual_Calculator/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Input;
+namespace Visual_Calculator
+{
+
+
+    public partial class FrmCalculator : Form
+    {
+      
+        public FrmCalculator()
+        {
+            InitializeComponent();
+        }
+    }
+
+    
+}
